06ac4beb6e96de5d9687b5dde95dbfc33ca99097
yankfy <yankfy@qq.com>
2018-12-20 14:56:16 +0800

    add second

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <div class="icon"><img class="icon-img" :src="image"></div>
+    <div class="title">SongTaste</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  data () {
+    return {
+      image: require('common/image/default.png')
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.header
+  display: flex
+  justify-content:center
+  color: #C6A300
+  height: 2.5rem
+  line-height: 2.5rem
+  .icon
+    height: 2.5rem
+    .icon-img
+      width: 2rem
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-    </div>
+  <div>
+    <home-header></home-header>
+    <home-tab></home-tab>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/header.vue'
+import HomeTab from './components/tab.vue'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader: HomeHeader,
+    HomeTab: HomeTab
+  }
 }
 </script>
 <style>
